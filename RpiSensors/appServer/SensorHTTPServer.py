5c43db8c2270ea0c8f61a88c734b7b4d53ec2442
akameswaran <akameswaran@gmail.com>
2014-07-08 08:39:47 -0400

    Initial

---
+++ RpiSensors/appServer/SensorHTTPServer.py
@@ -0,0 +1,23 @@
+'''
+Created on Jul 8, 2014
+
+@author: ak
+'''
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+import threading
+
+class 
+
+
+class SensorHttpServer(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
