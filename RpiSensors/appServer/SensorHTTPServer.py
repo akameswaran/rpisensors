4d47308b7bd31fffa5086a6dd0e797abfb4f5f98
akameswaran <akameswaran@gmail.com>
2014-07-08 11:09:47 -0400

    moving machines

--- RpiSensors/appServer/SensorHTTPServer.py
+++ RpiSensors/appServer/SensorHTTPServer.py
@@ -6,18 +6,42 @@ Created on Jul 8, 2014
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 import threading
+import urlparse
 
-class 
+class APIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse.urlparse(self.path)
+        message_parts = [
+                'CLIENT VALUES:',
+                'client_address=%s (%s)' % (self.client_address,
+                                            self.address_string()),
+                'command=%s' % self.command,
+                'path=%s' % self.path,
+                'real path=%s' % parsed_path.path,
+                'query=%s' % parsed_path.query,
+                'request_version=%s' % self.request_version,
+                '',
+                'SERVER VALUES:',
+                'server_version=%s' % self.server_version,
+                'sys_version=%s' % self.sys_version,
+                'protocol_version=%s' % self.protocol_version,
+                '',
+                'HEADERS RECEIVED:',
+                ]
+        for name, value in sorted(self.headers.items()):
+            message_parts.append('%s=%s' % (name, value.rstrip()))
+        message_parts.append('')
+        message = '\r\n'.join(message_parts)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(message)
+        return
 
 
-class SensorHttpServer(object):
+class SensorHttpServer(ThreadingMixIn, HTTPServer):
     '''
     classdocs
     '''
 
 
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
         
